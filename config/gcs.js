7c1a73e16b2b257eff18ded1953d34a8f6779fb3
govindjee725 <govindjee725@gmail.com>
2026-03-31 01:55:57 +0530

    fix

--- config/gcs.js
+++ config/gcs.js
@@ -1,17 +1,10 @@
-
 require('dotenv').config();
 const { Storage } = require('@google-cloud/storage');
-require('dotenv').config();
-
-const credentials = JSON.parse(process.env.GCP_KEY);
-
-// 🔥 FIX PRIVATE KEY NEWLINE ISSUE
-credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
 
 const storage = new Storage({
-  credentials,
+  keyFilename: process.env.GCP_KEY_PATH, // ✅ correct
 });
 
-const bucket = storage.bucket('YOUR_BUCKET_NAME');
+const bucket = storage.bucket(process.env.BUCKET_NAME);
 
 module.exports = bucket;
